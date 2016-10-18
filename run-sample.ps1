9ecb449dced57cc0340cf43b1c93ba3c6db5b36d
Samuli Seppänen <samuli@openvpn.net>
2016-10-18 09:12:28 +0300

    Update run-sample.ps1
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- run-sample.ps1
+++ run-sample.ps1
@@ -1,13 +1,15 @@
 ﻿$configdir = "C:\Program Files\OpenVPN\config"
 $basecmd = ".\Test-OpenVPN.ps1"
-$tests = "-TestCmdexe -TestGui -TestService"
 
-Function do_test([string]$configname, [array]$ping) {
+Function do_test([string]$tests, [string]$configname, [array]$ping) {
     $cmdline = "`"${basecmd}`" ${tests} -Config `"${configdir}\${configname}`" -Ping ${ping}"
     Invoke-Expression "& $cmdline"
 
 }
 
-do_test "company.ovpn" "10.10.112.100"
-do_test "university.ovpn" "10.8.21.9, fd00:feed:51:3::1"
-do_test "home.ovpn" "192.168.0.81"
+$tests = "-TestCmdexe"
+do_test "$tests" "university.ovpn" "10.8.21.9, fd00:feed:51:3::1"
+
+$tests = "-TestCmdexe -TestGui -TestService"
+do_test "$tests" "company.ovpn" "10.10.112.100"
+do_test "$tests" "home.ovpn" "192.168.0.81"
